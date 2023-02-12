bfe3ccb41d29ec1a7752a050fdf5ca516d63125d
nphuonha2101 <nphuonha2101@gmail.com>
2023-02-12 10:52:07 +0700

    second commit

--- src/main/java/project/mediaplayer/UI/MediaPlayerController.java
+++ src/main/java/project/mediaplayer/UI/MediaPlayerController.java
@@ -1,11 +1,15 @@
 package project.mediaplayer.UI;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class MediaPlayerController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Button button;
 
     @FXML
     protected void onHelloButtonClick() {
