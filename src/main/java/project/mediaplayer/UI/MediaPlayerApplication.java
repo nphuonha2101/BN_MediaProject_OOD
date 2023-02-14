207adb78da8d4ef42bb4b8c1e3faa0d21c94e7d0
bangpham <uzumakinarutoshin@gmail.com>
2023-02-14 23:07:01 +0700

    bang-icon

--- src/main/java/project/mediaplayer/UI/MediaPlayerApplication.java
+++ src/main/java/project/mediaplayer/UI/MediaPlayerApplication.java
@@ -3,6 +3,7 @@ package project.mediaplayer.UI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,9 +14,13 @@ public class MediaPlayerApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MediaPlayerApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 756);
+//        Image MediaPlayerApplication;
+//        stage.getIcons().add(new Image(MediaPlayerApplication.class.getResourceAsStream("/path/to/logo.png")));
+        stage.getIcons().add(new Image("https://drive.google.com/file/d/1A0jNwsp4SidYqEQoTT-88xCO5JZ5EZjb/view?usp=share_link"));
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
