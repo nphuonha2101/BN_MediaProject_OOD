a2ab10e24cac24789fd57e2ce07bdc9994ce7873
nphuonha2101 <nphuonha2101@gmail.com>
2023-02-14 23:33:17 +0700

    Nguyen Phuong Nha - new UI - 3

--- src/main/java/project/mediaplayer/UI/MediaPlayerApplication.java
+++ src/main/java/project/mediaplayer/UI/MediaPlayerApplication.java
@@ -3,7 +3,10 @@ package project.mediaplayer.UI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.IOException;
 
 
@@ -13,9 +16,13 @@ public class MediaPlayerApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MediaPlayerApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 756);
-        stage.setTitle("Hello!");
+        String basePath = new File("").getCanonicalPath();
+        basePath = basePath.replace("\\", "/") + "/src/main/resources/img/logo_title.png";
+        stage.setTitle("B&N Media Player");
+        stage.getIcons().add(new Image(basePath));
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
