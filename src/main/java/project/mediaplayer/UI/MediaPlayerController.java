46fe69254c96f1cc323a79ff488a82bb8785b029
bangpham <uzumakinarutoshin@gmail.com>
2023-03-03 14:00:39 +0700

    Add Progress Bar

--- src/main/java/project/mediaplayer/UI/MediaPlayerController.java
+++ src/main/java/project/mediaplayer/UI/MediaPlayerController.java
@@ -4,25 +4,22 @@ package project.mediaplayer.UI;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import project.mediaplayer.model.*;
 
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.ResourceBundle;
 import java.util.StringTokenizer;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MediaPlayerController implements Initializable {
+public class MediaPlayerController {
 
     @FXML
     private Button aboutButton;
@@ -49,15 +46,6 @@ public class MediaPlayerController implements Initializable {
 
     @FXML
     private ToggleButton favoriteSongButton;
-//    @FXML
-//    private  ToggleButton previousButton;
-//    @FXML
-//    private ToggleButton playButton;
-//    @FXML
-//    private ToggleButton nextButton;
-
-//    @FXML
-//    private Label songNameLabel;
 
     private final MainPlaylist mainPlaylist = new MainPlaylist(Playlists.MAIN_PLAYLIST);
     private final FavoritePlaylist favoritePlaylist = new FavoritePlaylist(Playlists.FAVORITE_PLAYLIST);
@@ -72,15 +60,19 @@ public class MediaPlayerController implements Initializable {
     @FXML
     private Label songNameLabel;
     @FXML
+    private ComboBox<String> speedBox;
+    @FXML
     private ListView<String> listView;
     @FXML
     private ToggleButton previousButton;
     @FXML
     private ToggleButton playButton;
     @FXML
+    private ProgressBar songProgressBar;
+    @FXML
     private ToggleButton nextButton;
-    //    @FXML
-//    private ToggleButton resetButton;
+    @FXML
+    private ToggleButton resetButton;
     private File directory;
     private File[] files;
     private ArrayList<File> songs;
@@ -88,46 +80,127 @@ public class MediaPlayerController implements Initializable {
     private Media media;
     private MediaPlayer mediaPlayer;
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        System.out.println(currentPlaylist.getPlaylistName());
+    private Timer timer;
+    private TimerTask task;
 
-        songs = new ArrayList<File>();
-        directory = new File("src/main/resources/music");
-        files = directory.listFiles();
+    private boolean running;
 
-        if (files != null) {
-            for (File file : files) {
-                songs.add(file);
-                System.out.println(file);
+
+//    private void loadSongs() {
+//        songs = new ArrayList<File>();
+//        directory = new File("src/main/resources/music");
+//
+//        if (directory.exists() && directory.isDirectory()) {
+//            files = directory.listFiles();
+//
+//            if (files != null) {
+//                for (File file : files) {
+//                    songs.add(file);
+//                    System.out.println(file);
+//                }
+//            }
+//        }
+//
+//        // Reload the media player with the new list of songs
+//        reloadMediaPlayer();
+//    }
+
+
+    private void initMediaPlayer(File folder) {
+        System.out.println("init");
+        songs = new ArrayList<>();
+        if (folder != null && folder.isDirectory()) {
+            files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    songs.add(file);
+                    System.out.println(file);
+                }
             }
         }
-
         media = new Media(songs.get(songNumber).toURI().toString());
         mediaPlayer = new MediaPlayer(media);
-
         songNameLabel.setText(songs.get(songNumber).getName());
+
+        songProgressBar.setStyle("-fx-accent: #00FF00;");
     }
 
 
+    //    @FXML
+//    protected void chooseFile() {
+//        Files files = new Files();
+//        files.clearAll();
+//        DirectoryChooser directoryChooser = new DirectoryChooser();
+//        directoryChooser.setTitle("Open Music Folder");
+//        File directory = directoryChooser.showDialog(null);
+//        if (directory != null) {
+//            for (File file : directory.listFiles()) {
+//                files.addFile(file);
+//            }
+//        }
+//
+//        mainPlaylist.addSongs(files);
+//        favoritePlaylist.addSongToFavorite(mainPlaylist);
+//        addToListView();
+//        loadSongs();
+//    }
+    @FXML
+    protected void chooseFile() {
+        Files files = new Files();
+        files.clearAll();
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Open Music Folder");
+        File folder = directoryChooser.showDialog(null);
+        if (folder != null) {
+            initMediaPlayer(folder);
+            for (File file : folder.listFiles()) {
+                files.addFile(file);
+            }
+            mainPlaylist.addSongs(files);
+
+            System.out.println(currentPlaylist.getPlaylistName());
+            addToListView();
+        }
+    }
+
+//    private void reloadMediaPlayer() {
+//        if (songs.isEmpty()) {
+//            // Handle the case when there are no songs to play
+//            return;
+//        }
+//
+//        // Reload the media player with the new list of songs
+//        media = new Media(songs.get(songNumber).toURI().toString());
+//        mediaPlayer = new MediaPlayer(media);
+//        songNameLabel.setText(songs.get(songNumber).getName());
+//    }
+
     public void playMedia() {
+
         if (playButton.isSelected()) {
+            beginTimer();
             System.out.println("Checkbox is selected");
             mediaPlayer.play();
         } else if (!playButton.isSelected()) {
             System.out.println("Checkbox is not selected");
+            cancelTimer();
             mediaPlayer.pause();
         }
     }
 
-//    public void resetMedia() {
-//        mediaPlayer.seek(Duration.seconds(0));
-//    }
+    public void resetMedia() {
+        songProgressBar.setProgress(0);
+        mediaPlayer.seek(Duration.seconds(0));
+    }
 
     public void previousMedia() {
         if (songNumber > 0) {
             songNumber--;
             mediaPlayer.stop();
+            if (running) {
+
+                cancelTimer();
+            }
             media = new Media(songs.get(songNumber).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
             songNameLabel.setText(songs.get(songNumber).getName());
@@ -135,6 +208,10 @@ public class MediaPlayerController implements Initializable {
         } else {
             songNumber = songs.size() - 1;
             mediaPlayer.stop();
+            if (running) {
+
+                cancelTimer();
+            }
             media = new Media(songs.get(songNumber).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
             songNameLabel.setText(songs.get(songNumber).getName());
@@ -144,40 +221,66 @@ public class MediaPlayerController implements Initializable {
 
     public void nextMedia() {
         if (songNumber < songs.size() - 1) {
+
             songNumber++;
+
             mediaPlayer.stop();
+
+            if (running) {
+
+                cancelTimer();
+            }
+
             media = new Media(songs.get(songNumber).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
-            songNameLabel.setText(songs.get(songNumber).getName());
+
+
             playMedia();
         } else {
+
             songNumber = 0;
+
             mediaPlayer.stop();
+
             media = new Media(songs.get(songNumber).toURI().toString());
             mediaPlayer = new MediaPlayer(media);
-            songNameLabel.setText(songs.get(songNumber).getName());
+
+
             playMedia();
         }
+    }
+
+    public void beginTimer() {
+
+        timer = new Timer();
+
+        task = new TimerTask() {
+
+            public void run() {
+
+                running = true;
+                double current = mediaPlayer.getCurrentTime().toSeconds();
+                double end = media.getDuration().toSeconds();
+                songProgressBar.setProgress(current / end);
+
+                if (current / end == 1) {
+
+                    cancelTimer();
+                }
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 1000);
+    }
+
+    public void cancelTimer() {
+
+        running = false;
+        timer.cancel();
     }
 
     // End Test ------------------------------------------------------------
-    @FXML
-    protected void chooseFile() {
-        Files files = new Files();
-        files.clearAll();
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        directoryChooser.setTitle("Open Music Folder");
-        File directory = directoryChooser.showDialog(null);
-        if (directory != null) {
-            for (File file : directory.listFiles()) {
-                files.addFile(file);
-            }
-        }
 
-        mainPlaylist.addSongs(files);
-        favoritePlaylist.addSongToFavorite(mainPlaylist);
-        addToListView();
-    }
 
     @FXML
     protected void mainPlaylistLView() {
@@ -240,8 +343,4 @@ public class MediaPlayerController implements Initializable {
         result = tokenizer.nextToken();
         return result;
     }
-
-
-
-
 }
